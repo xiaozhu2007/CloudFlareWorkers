143644b940994549c62e5c8a6d1286bf5b5637ea
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-06-06 22:44:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,118 @@
-const base = 'https://git.pig2333.workers.dev';
-const statusCode = 302;
+/**
+ * Main worker entry point.
+ */
+addEventListener("fetch", event => {
+  // Fail-safe in case of an unhandled exception
+  cons.i(`Request URL: ${event.request.url}`);
+  event.passThroughOnException();
+  event.respondWith(processRequest(event.request, event));
+});
 
-async function handleRequest(request) {
+/**
+ * Handle all non-proxied requests. Send HTML or CSS on for further processing
+ * and pass everything else through unmodified.
+ * @param {*} request - Original request
+ * @param {*} event - Original worker event
+ */
+async function processRequest(request, event) {
+  // Proxy the request
+  let init = {
+    method: request.method,
+    redirect: "manual",
+    headers: [...request.headers]
+  };
+  const clientAddr = request.headers.get('cf-connecting-ip');
+  if (clientAddr) {
+    init.headers['X-Forwarded-For'] = clientAddr;
+  }
   const url = new URL(request.url);
-  const { pathname, search } = url;
-  const destinationURL = base + pathname + search;
-  return Response.redirect(destinationURL, statusCode);
+  let proxyOrigin = url.origin;
+  const proxyUrl = 'https:/' + url.pathname + url.search;
+  let originalDomain = url.pathname.substr(1);
+  const domainEnd = originalDomain.indexOf('/');
+  if (domainEnd >= 0)
+    originalDomain = originalDomain.substr(0, domainEnd);
+  const response = await fetch(proxyUrl, init);
+  if (response) {
+    // Process test responses
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("text/") !== -1) {
+      let content = await response.text();
+
+      // Do the content-specific modification
+      content = rewriteProxyUrls(content, originalDomain, proxyOrigin);
+
+      // Create a cloned response with our modified body
+      let init = {
+        method: request.method,
+        headers: [...response.headers]
+      };
+      const newResponse = new Response(content, init);
+
+      // Return the in-process response so it can be streamed.
+      return newResponse;
+    }
+  }
+
+  return response;
 }
 
-addEventListener('fetch', async event => {
-  event.respondWith(handleRequest(event.request));
-});
+function rewriteProxyUrls(content, originalDomain, proxyOrigin) {
+  const hrefRegex = /href\s*=\s*['"]\s*((https?:)?\/?\/[^\s'"]+)\s*['"]/mig;
+  const srcRegex = /src\s*=\s*['"]\s*((https?:)?\/?\/[^\s'"]+)\s*['"]/mig;
+  const cssUrlRegex = /url\s*[\('"]*\s*((https?:)?\/?\/[^\s'"]+)\s*[\)'"]*/mig;
+  const importRegex = /@import\s*(url\s*)?[\('"\s]*((https?:\/)?\/[^'"\)]+)[\s'"\)]*\s*;/mig;
+  content = rewriteUrls(content, originalDomain, proxyOrigin, hrefRegex, 1);
+  content = rewriteUrls(content, originalDomain, proxyOrigin, srcRegex, 1);
+  content = rewriteUrls(content, originalDomain, proxyOrigin, cssUrlRegex, 1);
+  content = rewriteUrls(content, originalDomain, proxyOrigin, importRegex, 2);
+  return content;
+}
+
+function rewriteUrls(content, originalDomain, proxyOrigin, regex, group) {
+  let match = regex.exec(content);
+  while (match !== null) {
+    const url = match[group];
+    if (url && !url.startsWith(proxyOrigin)) {
+      let newUrl =null;
+      if (url.startsWith("//")) {
+        newUrl = proxyOrigin + url.substr(1);
+      } else if (url.startsWith("/") && !url.startsWith("/" + originalDomain)) {
+        newUrl = proxyOrigin + '/' + originalDomain + url;
+      } else {
+        let offset = url.indexOf('//');
+        if (offset >= 0) {
+          newUrl = proxyOrigin + url.substr(offset + 1);
+        }
+      }
+      if (newUrl !== null) {
+        let matchStr = match[0];
+        let newStr = matchStr.split(url).join(newUrl);
+        cons.i("Replacing: " + matchStr);
+        cons.i("     With: " + newStr);
+        content = content.split(matchStr).join(newStr);
+        regex.lastIndex -= matchStr.length - newStr.length;
+      }
+    }
+    match = regex.exec(content);
+  }
+  return content;
+}
+
+let cons = {
+    s: (m) => {
+        console.log(`%c[SUCCESS]%c ${m}`, 'color:white;background:green;', '')
+    },
+    w: (m) => {
+        console.log(`%c[WARNING]%c ${m}`, 'color:brown;background:yellow;', '')
+    },
+    i: (m) => {
+        console.log(`%c[INFO]%c ${m}`, 'color:white;background:blue;', '')
+    },
+    e: (m) => {
+        console.log(`%c[ERROR]%c ${m}`, 'color:white;background:red;', '')
+    },
+    d: (m) => {
+        console.log(`%c[DEBUG]%c ${m}`, 'color:white;background:black;', '')
+    }
+}
